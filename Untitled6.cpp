0e1f6a96fa42d2a3a45564f260fb51978b4739f0
Yurikamado <yeuhrur@gmail.com>
2024-10-21 08:46:11 +0700

    While uoc so.cpp

--- Untitled6.cpp
+++ Untitled6.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
 using namespace std;
+
 int main() {
-    double x;
-    cin >> x;
-    double result = sin(x) + sqrt(log(3 * x) / log(4)) + ceil(3 * exp(x));
-    cout << fixed << setprecision(6) << result << endl;
+    int n, i = 1;  // Khởi tạo biến n và biến đếm i
+    cout << "Nhap so nguyen duong n: ";
+    cin >> n;
+
+    cout << "Cac uoc so cua " << n << " la: ";
+    
+    // Sử dụng cấu trúc lặp while để tìm các ước số của n
+    while (i <= n) {
+        if (n % i == 0) {
+            cout << i << " ";  // In ra i nếu i là ước số của n
+        }
+        i++;  // Tăng biến đếm
+    }
+    cout << endl;
+
     return 0;
 }
